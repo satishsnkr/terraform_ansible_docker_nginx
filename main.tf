41fa2ee4ff91c6a7b75a7aba1bc16937784a43e2
Satish Sunkara <satishsunkara@Satishs-MacBook-Pro.local>
2021-09-21 10:35:06 +1200

    ADded Ansible module to start Nginx on docker container

--- main.tf
+++ main.tf
@@ -27,6 +27,11 @@ resource "aws_instance" "app_vm" {
   }
 }
 
+resource "aws_eip" "elastic_ip" {
+  instance = aws_instance.app_vm.id
+  vpc      = true
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -78,6 +83,29 @@ resource "aws_security_group" "vm_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+   egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
 }
 
 resource "random_string" "random" {
